46755a494e212a4f43a43db7846d5cdf3511f5da
SupriyaGangavarapu <102588287+SupriyaGangavarapu@users.noreply.github.com>
2023-06-20 15:31:59 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,14 @@
+package project1;
+public class Arrays {
+public static void main(String[] args) {
+int a[]= {1,3,5,7,9};
+for(int i=0;i<5;i++) {
+	System.out.println("elements of array a:"+a[i]);
+}
+int[][] b={ 
+		{2,3,5,6},
+		{5,6,3,4}};
+System.out.println("\nLength of row1:"+b[0].length);
+
+}
+}
