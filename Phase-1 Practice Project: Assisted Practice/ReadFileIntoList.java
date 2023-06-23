ede5aee68867a5b3311a46e5834d702cd6b36dd6
SupriyaGangavarapu <102588287+SupriyaGangavarapu@users.noreply.github.com>
2023-06-23 16:53:40 +0530

    Add files via upload

---
+++ Phase-1 Practice Project: Assisted Practice/ReadFileIntoList.java
@@ -0,0 +1,24 @@
+package project2;
+import java.util.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.io.*;
+
+public class ReadFileIntoList{
+	public static List<String> readFileInList(String fileName){
+	 List<String> lines=Collections.emptyList();
+	 try {
+		 lines=Files.readAllLines(Paths.get(fileName),StandardCharsets.UTF_8);
+	 }
+	 catch(IOException e) {
+		 e.printStackTrace();
+	 }
+	 return lines;
+	}
+	public static void main(String[] args){
+		List I=readFileInList("c//temp//testFile2.txt");
+		Iterator<String> itr=I.iterator();
+		while(itr.hasNext())
+			System.out.println(itr.next());
+	}
+}
