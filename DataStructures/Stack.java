aaa6b256f36c220c1e9c80def1c319cf3a7eeed8
SupriyaGangavarapu <102588287+SupriyaGangavarapu@users.noreply.github.com>
2023-06-27 17:23:14 +0530

    Add files via upload

---
+++ DataStructures/Stack.java
@@ -0,0 +1,42 @@
+package SortingTechniques;
+
+public class Stack {
+	static final int MAX=1000;
+	int top;
+	int a[]=new int[MAX];
+	boolean isEmpty() {
+		return(top<0);
+	}
+	Stack(){
+		top=-1;
+	}
+	boolean push(int x) {
+		if(top>=(MAX-1)) {
+			System.out.println("stack overflow");
+			return false;
+		}
+		else {
+			a[++top]=x;
+			System.out.println(x+"pushed into stack");
+			return true;
+		}
+	}
+	int pop() {
+		if(top<0) {
+			System.out.println("stack underflow");
+			return 0;
+		}else {
+			int x=a[top--];
+			return x;
+		}
+	}
+	public static void main(String[] args) {
+		Stack s=new Stack();
+		s.push(10);
+		s.push(15);
+		s.push(20);
+		System.out.println(s.pop()+"popped from stack");
+
+	}
+
+}
